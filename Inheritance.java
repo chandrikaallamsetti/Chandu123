dd2418e29aedd1991d1a68104991e13d3ed10474
chandrikaallamsetti <158675346+chandrikaallamsetti@users.noreply.github.com>
2024-03-07 06:56:48 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,30 @@
+class Public
+{
+public
+int publicVar=1;
+private
+int privateVar=2;
+protected
+int protectedVar=3;
+public
+int getPrivate()
+{
+    return privateVar;
+    }
+    }
+    /*private class Private
+    {
+    }*/
+   
+    public class Inheritance
+    {
+    public static void main(String[] args)
+    {
+    Public pub=new Public();
+    System.out.print("Public : "+pub.publicVar+"\n");
+    //System.out.print("Public : "+pub.privateVar);
+    //We cannot access the privateVar because it is private i.e, accessible only within the class.
+    System.out.print("Private : "+pub.getPrivate()+"\n");
+    System.out.print("Protected : "+pub.protectedVar+"\n");
+    }
+    }
