c475b13223a429990ccf5aeee6b24507fa10eef5
chandrikaallamsetti <158675346+chandrikaallamsetti@users.noreply.github.com>
2024-04-18 06:55:56 +0530

    Add files via upload

---
+++ MutableAndImmutable.java
@@ -0,0 +1,24 @@
+class Immutable{
+private final String a="Ravi Chandrika";
+Immutable(String a){
+//this.a=a;
+//This shows an error because we have given the variable as final means we cannot
+//and the value cannot be overridden.
+}
+void display(){
+System.out.println("Name : "+a);
+}
+}
+public class MutableAndImmutable {
+public static void main(String[] args) {
+//Mutable Object
+StringBuilder obj=new StringBuilder("Ravi");
+obj.append(" ");
+obj.append("Chandrika");
+String a=obj.toString();
+System.out.println("The final modified string : "+a);
+//Immutable object
+Immutable obj1=new Immutable("Deadpool");
+obj1.display();
+}
+}
